4ae493101620409b74ce219fb0a904455527db7c
Greem <orangenewyorkcreativeagency@gmail.com>
2020-04-28 04:14:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,11 @@
 def square_array(array)
   # your code here
-  array.length.times do |index|
-    array.unshift(4)
-    array.shift((array[index]) *( array[index]))
+  array.length.times do |index|#0,#1 
+  
+  array.push((array[index]) * (array[index])) 
+  
+  #array.shift(array[index] * array[index])
 end
+array.shift(2)
+array
 end
